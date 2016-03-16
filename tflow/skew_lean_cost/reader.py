0ee75317dad40f705416441933f20a0906f26d15
ofer <oferyo@gmail.com>
2016-03-16 15:39:45 +0200

    tflow initial files

--- tflow/skew_lean_cost/reader.py
+++ tflow/skew_lean_cost/reader.py
@@ -3,13 +3,17 @@ import numpy as np
 
 class DataReader(object):
 
-    def __init__(self, batch_size, from_date, to_date):
+    def __init__(self, batch_size, from_date, to_date, limit = -1):
         self.raw_data = []
         conn = pymssql.connect("192.168.122.200", "sa","a:123456a:123456" , "Goldfish")
         cursor = conn.cursor(as_dict=True)
-        num_features = 11
-        max_rows = 85000
-        cursor.execute('SELECT top 85000 * FROM ExePairs WHERE ExeDate>%s  and ExeDate < %s and IvPnlOpen is not null and IvPnlClose is not null  and BuyOpen != BuyClose order by ExeDate', (from_date, to_date))
+        num_features = 13
+        max_rows = 300000
+        if limit < 0:
+            sql = 'SELECT  * FROM ExePairs WHERE ExeDate>%s  and ExeDate < %s and Pnl is not null and ExpiryMonthOpen = ExpiryMonthClose and IvPnlClose is not null  and BuyOpen != BuyClose and BidAskWidthMultOpen is not null order by ExeDate'
+        else:
+            sql = 'SELECT  top ' +  str(2*limit) + ' * FROM ExePairs WHERE ExeDate>%s  and ExeDate < %s and Pnl is not null and ExpiryMonthOpen = ExpiryMonthClose and IvPnlClose is not null  and BuyOpen != BuyClose and BidAskWidthMultOpen is not null order by ExeDate'
+        cursor.execute(sql, (from_date, to_date))
         next_batch = np.zeros((batch_size, num_features), float)
         next_targets = np.zeros((batch_size, 1), float)
         ind = 0
@@ -19,22 +23,37 @@ class DataReader(object):
         raw_output = np.zeros((max_rows, 1), float)
         raw_ind = 0
         for row in cursor:
-            totalRows+=1
+            if totalRows == limit:
+                break
             next_batch[ind, 0] = row[u'Kasasab']
             next_batch[ind, 1] = row[u'BidAskWidthMultOpen']
             sqrtt = np.sqrt(row[u'ExpiryOpen'] / 365.0)
+            distance = self.distance(row)
             next_batch[ind, 2] = sqrtt
             next_batch[ind, 3] = self.diff_in_iv_point(row, True)
             next_batch[ind, 4] = self.diff_in_iv_point(row, False)
-            next_batch[ind, 5] = self.distance(row)
+            next_batch[ind, 5] = distance
             next_batch[ind, 6] = np.abs(row[u'VegaOpen'])
             next_batch[ind, 7] = np.abs(row[u'VegaClose'])
-            next_batch[ind, 8] = np.abs(row[u'ExecutedDelta'])
-            next_batch[ind, 9] = np.abs(row[u'VegaOpen']/row[u'DeltaOpen'])
-            next_batch[ind, 10] = np.abs(row[u'VegaClose']/row[u'DeltaClose'])
+            #next_batch[ind, 8] = np.abs(row[u'ExecutedDelta'])
+            # next_batch[ind, 8] = np.abs(row[u'VegaOpen']/)
+            vodo = np.abs(row[u'VegaOpen']) / np.abs(row[u'DeltaOpen'])
+            vcdc = np.abs(row[u'VegaClose']) / np.abs(row[u'DeltaClose'])
+            next_batch[ind, 8] = vcdc
+            next_batch[ind, 9] = vodo
+            # next_batch[ind, 11] = np.abs(row[u'DeltaClose'])
 
-            # next_targets[ind ,0] = 100.0 * (row[u'IvPnlOpen'] + row[u'IvPnlClose'])
-            next_targets[ind ,0] = row[u'Pnl']
+            next_batch[ind, 10] = row[u'BidAskWidthMultClose']
+            # next_batch[ind, 11] = row[u'ExecutedDelta']
+            hx = np.sqrt((np.abs(vodo-vcdc) ** 2) + (distance**2)*(vcdc**2))
+            un_norm = max(hx * np.abs(row[u'ExecutedDelta']), 1e-8)
+            if un_norm < 1e-6:
+                continue
+            totalRows+=1
+            next_batch[ind, 11] = un_norm
+            next_batch[ind, 12] = (hx**2) * (row[u'ExecutedDelta'] **2)
+            next_targets[ind ,0] = row[u'Pnl'] / un_norm
+            # next_targets[ind ,0] = row[u'Pnl']
             raw_data[raw_ind] = next_batch[ind]
             raw_output[raw_ind] = next_targets[ind ,0]
             ind+=1
@@ -44,6 +63,7 @@ class DataReader(object):
                 next_batch = np.zeros((batch_size, num_features), float)
                 next_targets = np.zeros((batch_size, 1), float)
                 ind = 0
+
 
         raw_data = raw_data[0:totalRows, :]
         raw_output = raw_output[0:totalRows, :]
@@ -60,7 +80,7 @@ class DataReader(object):
 
         conn.close()
 
-        print ("finished totalRows", totalRows, "std_out", std_out, "mean_out", mean_out, "std_in", std, "mean_in", mean)
+        print ("finished totalRows", totalRows, "std_out", std_out, "mean_out", mean_out)
 
 
     def date_iterator(self):
@@ -81,10 +101,15 @@ class DataReader(object):
         return self.diff_in_iv_point(row, True) - self.diff_in_iv_point(row, False)
 
     def normalize(self, batch_in):
-        return (batch_in - self._mean_in) / np.max(self._std_in, 1e-12)
+        return (batch_in - self._mean_in) / self._std_in
 
     def normalize_out(self, out):
-        return (out - self._mean_out) / np.max(self._std_out, 1e-8)
+        return (out - self._mean_out) / self._std_out
+        # return out
 
     def de_normalize_out(self, pred):
-        return (pred * np.max(self._std_out, 1e-8)) + self._mean_out
+        return (pred * self._std_out) + self._mean_out
+        # return pred
+
+    def mean_out(self):
+        return self._mean_out;
--- tflow/skew_lean_cost/skew_lean_cost.py
+++ tflow/skew_lean_cost/skew_lean_cost.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import reader as reader
+import pymssql
 from datetime import date, timedelta, datetime
 class NNModel(object):
 
@@ -9,6 +10,7 @@ class NNModel(object):
 
       batch_size = config.batch_size
       num_hidden = config.num_hidden
+      self._cost_coeff = tf.placeholder(tf.float32, (batch_size, 1))
       self._input_data = tf.placeholder(tf.float32, (batch_size, num_features))
       self._targets = tf.placeholder(tf.float32, [batch_size, 1])
 
@@ -24,14 +26,20 @@ class NNModel(object):
       self._pred = pred
 
       self._result = pred - self._targets
-      self._cost = cost = tf.reduce_mean(tf.square(pred[:,0] - self._targets[:,0]))
+      self._cost  = tf.reduce_mean(tf.square(pred[:,0] - self._targets[:,0]) * self._cost_coeff)
+
+      regularization_param = 0.1
+
+      l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
+      self._cost = self._cost +  regularization_param * l2_loss
+      cost = self._cost
+
       self._train_op = tf.no_op()
 
       if is_training:
           # optimizer = tf.train.GradientDescentOptimizer(learning_rate = config.learning_rate).minimize(cost)
           optimizer = tf.train.AdamOptimizer().minimize(cost)
           self._train_op = optimizer
-
 
   @property
   def input_data(self):
@@ -57,65 +65,107 @@ class NNModel(object):
   def pred(self):
     return self._pred
 
-def run_epoch(session, data_reader, normalizer, model, config, train_op):
+  @property
+  def cost_coeff(self):
+    return self._cost_coeff
+
+def run_epoch(session, data_reader, normalizer, model, config, train_op, mean_out):
     naive_cost = 0
     total_seq = 0
     dynamic_cost = 0
     total_batches = 0
+    sum_gaps = 0
     for step, (x, y) in enumerate(data_reader.date_iterator()):
 
           nx = normalizer.normalize(x)
           ny = normalizer.normalize_out(y)
+          cost_coeff = np.reshape(x[:,12], (config.batch_size,1))
           cost, pred, result, _ = session.run([model.cost, model.pred, model.result, train_op],
                                        {model.targets: ny,
+                                        model.cost_coeff : cost_coeff,
                                         model.input_data : nx})
           total_batches+=1
           total_seq+=config.batch_size
           uny = normalizer.de_normalize_out(pred)
-          dynamic_cost+= np.sum(np.square(uny - y)) #undo the sqrtt mult
-          naive_cost+= np.sum(np.square(y))
-    return dynamic_cost / total_seq, naive_cost / total_seq
+          # sqrtt = np.reshape(x[:,2], (config.batch_size,1))
 
+          un_norm =   np.reshape(x[:,11], (config.batch_size,1))
+          gap = np.abs((uny - y) * un_norm)
+          # gap = np.abs((uny - y))
 
-def main(unused_args):
+          sum_gaps+= np.sum(gap)
+          dynamic_cost+= np.sum(np.square(gap)) #undo the sqrtt mult
+          naive_cost+= np.sum(np.square((y - mean_out)))
+    return dynamic_cost / naive_cost , sum_gaps, total_seq
+
+def run_configuration(from_date, to_date, out_from_date, out_to_date):
 
   with tf.Graph().as_default(), tf.Session() as session:
     config = BaseConfig()
     std = 1.0 / np.sqrt(config.num_hidden)
+    # std = 0.01
     initializer = tf.random_normal_initializer(0.0, std, None)
-    max_epoch = 10000
-
-    from_date = datetime.now() - timedelta(days=30)
-    to_date = datetime.now() - timedelta(days=1)
-
-    days = 10
-    out_from_date = datetime.now() - timedelta(days=39)
-    out_to_date = datetime.now() - timedelta(days=31)
-    data_reader = reader.DataReader(config.batch_size, from_date, to_date)
-    data_reader_out = reader.DataReader(config.batch_size, out_from_date, out_to_date)
+    max_epoch = 200
+    limit = 500
+    data_reader_in = reader.DataReader(config.batch_size, from_date, to_date)
+    data_reader_out = reader.DataReader(config.batch_size, out_from_date, out_to_date, limit)
 
     with tf.variable_scope("model", reuse=None, initializer=initializer):
         model = NNModel(True, config)
 
     tf.initialize_all_variables().run()
+    best_out = 2.0
+    best_sum_out = 1e+6
+    in_rss = 2.0
+    best_iteration = 0
+
+    # cost, naive_cost, total_seq = run_epoch(session, data_reader_in, data_reader_in, model, config, tf.no_op())
+    # t_cost, t_naive_cost, t_total_seq = run_epoch(session, data_reader_out, data_reader_in, model, config, tf.no_op())
+    # print ("first is ", (cost/naive_cost), "first test", (t_cost/t_naive_cost))
+
     for i in range(max_epoch):
-        cost, naive_cost = run_epoch(session, data_reader, data_reader, model, config, model.train_op)
-        if i % 50 == 0:
-            print ("cost", cost, "naivecost", naive_cost, "relative_cost", (cost/naive_cost), "i", i)
+        in_rss, sum_in, total_seq = run_epoch(session, data_reader_in, data_reader_in, model, config, model.train_op, data_reader_in.mean_out())
+        out_rss, sum_out, total_seq_out = run_epoch(session, data_reader_out, data_reader_in, model, config, tf.no_op(), data_reader_in.mean_out())
 
-        if i % 50 ==0:
-            cost, naive_cost = run_epoch(session, data_reader_out, data_reader, model, config, tf.no_op())
-            if (cost < 1.0):
-                print ("test model ", cost, "ncost ", naive_cost, "relative", cost / naive_cost, "i", i)
+        # if i % 10 == 0:
+        #     print ("in", (cost/naive_cost), "out", (t_cost/t_naive_cost), "i", i)
 
+        if (sum_out < best_sum_out):
+            best_out = out_rss
+            best_sum_out = sum_out
+            best_iteration = i
+                # print ("test model ", cost, "ncost ", naive_cost, "relative", cost / naive_cost, "i", i)
+    last_out, last_sum_out, total_seq_out = run_epoch(session, data_reader_out, data_reader_in, model, config, tf.no_op(), data_reader_in.mean_out())
 
-    cost, naive_cost = run_epoch(session, data_reader_out, data_reader, model, config, tf.no_op())
-    print ("end test model ", cost, "ncost ", naive_cost, "relative", cost / naive_cost)
+    print ("best_iteration ", best_iteration, "total_sql", total_seq_out)
+    return best_out, best_sum_out, last_out,last_sum_out, in_rss, total_seq_out
 
+def get_min_date():
+    conn = pymssql.connect("192.168.122.200", "sa","a:123456a:123456" , "Goldfish")
+    cursor = conn.cursor(as_dict=True)
+    cursor.execute('SELECT top 1 ExeDate FROM ExePairs WHERE BidAskWidthMultOpen is not null order by ExeDate')
+    for row in cursor:
+        return row[u'ExeDate']
+
+    return datetime.min
+
+def main(unused_args):
+
+    days_in = 30
+    days_out = 20
+    from_date = get_min_date()
+
+    while from_date < datetime.now():
+        to_date = from_date + timedelta(days=days_in)
+        from_out = to_date + timedelta(hours=1)
+        to_out = from_out + timedelta(days=days_out)
+        best_out, best_sum_out, last_out, last_sum_out, last_in, count_out_samples = run_configuration(from_date, to_date, from_out, to_out)
+        print ("best_sum  ", best_sum_out, "  last_sum_out  ", last_sum_out, " last_out ", last_out, "  best_out  ", best_out, "in_rss", last_in, "count_out", count_out_samples, from_out, to_out)
+        from_date+= timedelta(days=days_out)
 
 class BaseConfig(object):
     batch_size = 500
-    num_features = 11
+    num_features = 13
     num_hidden = 3
     num_layers = 1
     learning_rate = 0.2
